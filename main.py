e58de63bf3725f040540f4a6705c57c257e498fb
MEHRANSHAHBAAZI <MEHRANSHAHBAAZI96@gmail.com>
2026-02-12 20:06:25 +0100

    basic project added

---
+++ main.py
@@ -0,0 +1,56 @@
+from fileinput import filename
+import os
+from flask import Flask,render_template, request, redirect, url_for
+app = Flask(__name__)
+UPLOAD_FOLDER = "static/uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+carlist = [
+   {"name": "Viper", "brand": "Dodge", "year": 2020, "price": 80000 , "img": "viper.png"},
+   {"name": "Mustang", "brand": "Ford", "year": 2019, "price": 25000 , "img": "mustang.jpeg"},
+   {"name": "Camaro", "brand": "Chevrolet", "year": 2021, "price": 30000 , "img": "camaro.jpg"},
+   {"name": "A200", "brand": "Mercedes-Benz", "year": 2021, "price": 35000 , "img": "a200.jpg"},
+   ]
+
+@app.route("/")
+def home():
+    return get_template()
+
+@app.route("/cars", methods=["GET"])
+def cars():
+    return get_template()
+
+@app.route("/search", methods=["GET"])
+def search():
+    if request.method == "GET":
+        return get_template()
+    
+    query = request.form.get("query", "")
+    results = [car for car in carlist if query.lower() in car["name"].lower()]
+    return render_template("index.html", cars=results)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    brand = request.form["brand"]
+    name = request.form["name"]
+    year = int(request.form["year"])
+    price = int(request.form["price"])
+    file = request.files["img"]
+    if file:
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filepath)
+        img_path = f"uploads/{file.filename}"
+    else:
+        img_path = "default.jpg"
+    new_car = {"name": name, "brand": brand, "year": year, "price": price, "img": img_path}
+    carlist.append(new_car)
+    return get_template()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=8000, debug=True)
+
+def get_template():
+    return render_template("index.html", cars=carlist)
