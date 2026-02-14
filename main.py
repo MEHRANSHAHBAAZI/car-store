b0d15c0ba9617910dd20102c9b8bac270b872257
apourhadi <apourhadi@e-470.com>
2026-02-14 23:35:57 +0100

    - static data moved to cars.py - adding car from html code changed to js - css style added to cars list - error handling improved(it's general now)

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 from fileinput import filename
 import os
-from flask import Flask,render_template, request, redirect, url_for
+from flask import Flask,render_template, request, redirect, url_for, jsonify
+from werkzeug.exceptions import HTTPException
+from static_data.cars import carlist
+
 app = Flask(__name__)
 
 load_dotenv()
@@ -10,26 +13,16 @@ upload_folder = os.getenv("UPLOAD_FOLDER")
 app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024  
 os.makedirs(upload_folder, exist_ok=True)
 
-@app.errorhandler(413)
-def file_too_large(error):
-    return "file is too large (maxximum 1MB)", 413
-
-carlist = [
-   {"name": "Viper", "brand": "Dodge", "year": 2020, "price": 80000 , "img": "viper.png"},
-   {"name": "Mustang", "brand": "Ford", "year": 2019, "price": 25000 , "img": "mustang.jpeg"},
-   {"name": "Camaro", "brand": "Chevrolet", "year": 2021, "price": 30000 , "img": "camaro.jpg"},
-   {"name": "A200", "brand": "Mercedes-Benz", "year": 2021, "price": 35000 , "img": "a200.jpg"},
-   ]
-
-def get_template():
-    return render_template("index.html", cars=carlist)
+@app.errorhandler(HTTPException)
+def handle_http_exception(error):
+    return jsonify({
+        "error": error.name,
+        "message": error.description,
+        "status_code": error.code
+    }), error.code
 
 @app.route("/")
 def home():
-    return get_template()
-
-@app.route("/cars", methods=["GET"])
-def cars():
     return render_template("index.html", cars=carlist)
 
 @app.route("/search", methods=["GET"])
@@ -48,15 +41,21 @@ def add():
     year = int(request.form["year"])
     price = int(request.form["price"])
     file = request.files["img"]
+
     if file:
         filepath = os.path.join(upload_folder, file.filename)
         file.save(filepath)
         img_path = f"uploads/{file.filename}"
     else:
         img_path = "default.jpg"
+
     new_car = {"name": name, "brand": brand, "year": year, "price": price, "img": img_path}
+
     carlist.append(new_car)
-    return get_template()
+
+    return jsonify({
+        "message": f"{name} {brand} added successfully."
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static_data/cars.py
@@ -0,0 +1,6 @@
+carlist = [
+   {"name": "Viper", "brand": "Dodge", "year": 2020, "price": 80000 , "img": "viper.png"},
+   {"name": "Mustang", "brand": "Ford", "year": 2019, "price": 25000 , "img": "mustang.jpeg"},
+   {"name": "Camaro", "brand": "Chevrolet", "year": 2021, "price": 30000 , "img": "camaro.jpg"},
+   {"name": "A200", "brand": "Mercedes-Benz", "year": 2021, "price": 35000 , "img": "a200.jpg"},
+]
